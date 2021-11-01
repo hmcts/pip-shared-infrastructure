0f372750999c8cd673113a52d72b5a3c04825de1
Christopher Pateman <christopher.pateman@version1.com>
2021-11-01 14:58:46 +0000

    update read me and environment

--- terraform/deployments/sharedinfra/main.tf
+++ terraform/deployments/sharedinfra/main.tf
@@ -105,8 +105,8 @@ module "dtu_sa" {
 
 data "azurerm_subnet" "postgres" {
   name                 = "iaas"
-  resource_group_name  = "ss-${var.env}-network-rg"
-  virtual_network_name = "ss-${var.env}-vnet"
+  resource_group_name  = "ss-${var.environment}-network-rg"
+  virtual_network_name = "ss-${var.environment}-vnet"
 }
 
 module "databases" {
