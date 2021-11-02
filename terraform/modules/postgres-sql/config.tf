9bebca0ed2f2a3994c7b0556068285662ab65ae0
Christopher Pateman <christopher.pateman@version1.com>
2021-11-02 11:20:46 +0000

    add config and split files

---
+++ terraform/modules/postgres-sql/config.tf
@@ -0,0 +1,18 @@
+locals {
+  settings_on  = ["log_checkpoints", "connection_throttling", "log_connections"]
+  settings_off = []
+}
+resource "azurerm_postgresql_configuration" "config_on" {
+  for_each            = { for settings_on in local.settings_on : settings_on => settings_on }
+  name                = each.value
+  resource_group_name = var.resource_group
+  server_name         = azurerm_postgresql_server.postgres-paas.name
+  value               = "on"
+}
+resource "azurerm_postgresql_configuration" "config_off" {
+  for_each            = { for settings_off in local.settings_off : settings_off => settings_off }
+  name                = each.value
+  resource_group_name = var.resource_group
+  server_name         = azurerm_postgresql_server.postgres-paas.name
+  value               = "off"
+}
