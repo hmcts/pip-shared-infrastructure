beee4123c7fab0b02644cfe42fad7e6fe430575d
Christopher Pateman <christopher.pateman@version1.com>
2021-11-02 10:29:43 +0000

    add postgres db

---
+++ terraform/modules/postgres-sql/main.tf
@@ -0,0 +1,90 @@
+
+
+
+resource "random_password" "password" {
+  length  = 16
+  special = true
+  upper   = true
+  lower   = true
+  number  = true
+}
+
+resource "azurerm_postgresql_server" "postgres-paas" {
+  name                = var.server_name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+
+  administrator_login          = var.postgresql_user
+  administrator_login_password = random_password.password.result
+
+  sku_name   = var.sku_name
+  version    = var.postgresql_version
+  storage_mb = var.storage_mb
+
+  backup_retention_days            = var.backup_retention_days
+  geo_redundant_backup_enabled     = var.georedundant_backup
+  ssl_enforcement_enabled          = true
+  ssl_minimal_tls_version_enforced = "TLS1_2"
+  public_network_access_enabled    = true
+
+  tags = var.common_tags
+}
+
+resource "azurerm_postgresql_database" "postgres-dbs" {
+  for_each            = { for database in var.database_names : database => database }
+  name                = replace(each.value, "-", "")
+  resource_group_name = var.resource_group_name
+  server_name         = azurerm_postgresql_server.postgres-paas.name
+  charset             = var.charset
+  collation           = var.collation
+}
+
+
+## Add details to Key Vault
+locals {
+  postgresql_prefix = "postgre"
+}
+data "azurerm_key_vault" "ss_kv" {
+  name                = "${var.product}-shared-kv-${var.environment}"
+  resource_group_name = "${var.product}-sharedservices-${var.environment}-rg"
+}
+module "keyvault_postgre_secrets" {
+  source = "../key-vault/secret"
+
+  key_vault_id = data.azurerm_key_vault.ss_kv.id
+  tags         = var.common_tags
+  secrets = [
+    {
+      name  = "${local.postgresql_prefix}-host"
+      value = "${azurerm_postgresql_server.postgres-paas.name}.postgres.database.azure.com"
+      tags = {
+        "source" : "PostgreSQL"
+      }
+      content_type = ""
+    },
+    {
+      name  = "${local.postgresql_prefix}-port"
+      value = var.postgresql_listen_port
+      tags = {
+        "source" : "PostgreSQL"
+      }
+      content_type = ""
+    },
+    {
+      name  = "${local.postgresql_prefix}-user"
+      value = var.postgresql_user
+      tags = {
+        "source" : "PostgreSQL"
+      }
+      content_type = ""
+    },
+    {
+      name  = "${local.postgresql_prefix}-pwd"
+      value = random_password.password.result
+      tags = {
+        "source" : "PostgreSQL"
+      }
+      content_type = ""
+    }
+  ]
+}
