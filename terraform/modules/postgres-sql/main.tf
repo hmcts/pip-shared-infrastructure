32abf458a00e55b1d64098829c77d4b4f591a16a
Christopher Pateman <christopher.pateman@version1.com>
2021-11-02 10:44:50 +0000

    add postgres db admin

--- terraform/modules/postgres-sql/main.tf
+++ terraform/modules/postgres-sql/main.tf
@@ -30,6 +30,30 @@ resource "azurerm_postgresql_server" "postgres-paas" {
   tags = var.common_tags
 }
 
+locals {
+  is_prod     = length(regexall(".*(prod).*", var.environment)) > 0
+  admin_group = local.is_prod ? "DTS Platform Operations SC" : "DTS Platform Operations"
+  # psql needs spaces escaped in user names
+  escaped_admin_group = replace(local.admin_group, " ", "\\ ")
+}
+
+data "azurerm_client_config" "current" {}
+
+data "azuread_group" "db_admin" {
+  display_name     = local.admin_group
+  security_enabled = true
+}
+
+resource "azurerm_postgresql_active_directory_administrator" "admin" {
+  for_each            = { for database in azurerm_postgresql_database.postgres-dbs : database => database }
+  server_name         = each.value.server_name
+  resource_group_name = var.resource_group_name
+  login               = local.admin_group
+  tenant_id           = data.azurerm_client_config.current.tenant_id
+  object_id           = data.azuread_group.db_admin.object_id
+}
+
+
 resource "azurerm_postgresql_database" "postgres-dbs" {
   for_each            = { for database in var.database_names : database => database }
   name                = replace(each.value, "-", "")
